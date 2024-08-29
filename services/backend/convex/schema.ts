4b5b8bd1ae785f80d18b0f9011666b34ac343492
conradkoh <conradkoh@gmail.com>
2024-08-29 09:59:56 +0800

    infra: add prod cd

--- services/backend/convex/schema.ts
+++ services/backend/convex/schema.ts
@@ -1,5 +1,10 @@
-import { defineSchema } from "convex/server";
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
 
 export default defineSchema ({
-  
+  user: defineTable({
+    id: v.id("user"),
+    name: v.string(),
+    status: v.literal('active')
+  })
 });
